682d084d17363387ccd1a458911393cc73f620b8
Chris Flatley <chris@committed.io>
2021-10-22 13:44:56 +0000

    Add web bluetooth react app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from 'react';
 
-function App() {
+function useBluetoothDevice(options: RequestDeviceOptions = { acceptAllDevices: true }): [BluetoothDevice | undefined, () => void]  {
+  const [device, setDevice] = useState<BluetoothDevice | undefined>(undefined)
+  const requestDevice = useCallback(() => {
+    async function doRequest() {
+      const d = await navigator.bluetooth.requestDevice(options)
+      setDevice(d)
+    }
+    doRequest()
+  }, [setDevice, options])
+
+  return [device, requestDevice]
+}
+
+function useBluetoothServer(device?: BluetoothDevice): BluetoothRemoteGATTServer | undefined  {
+  const [server, setServer] = useState<BluetoothRemoteGATTServer | undefined>(undefined)
+
+  useEffect(() => {
+    async function doConnect() {
+      if(device && device.gatt) {
+        const server = await device.gatt.connect()
+        setServer(server)
+      } else {
+        setServer(undefined)
+      }
+    }
+
+    doConnect()
+  }, [device, setServer])
+
+  return server
+}
+
+export function App() {
+
+  const [device, request] = useBluetoothDevice()
+  const server = useBluetoothServer(device)
+
+  console.log(device)
+  console.log(server)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Web Bluetooth</h1>
+      <button onClick={request}>Connect to Bluetooth</button>
+      { device && <p>Device: {device.id}</p>}
+      { server && <p>Server: {server.connected ? "Connnected" : "Disconnected"}</p>}
     </div>
   );
 }
 
-export default App;
